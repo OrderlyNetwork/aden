91304e00fae2bd789f0d4d2458bf1a266ae61cbc
Wuzhong <wuzhong@orderly.network>
2025-10-15 19:15:19 +0800

    Setting Orderbook coin-type to USDC

--- app/routes/perp.$symbol.tsx
+++ app/routes/perp.$symbol.tsx
@@ -33,6 +33,29 @@ export default function PerpPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    let t: number | undefined;
+    if (location.pathname.startsWith("/perp")) {
+      t = window.setTimeout(() => {
+        try {
+          // For desktop orderbook - set to quote (USDC)
+          localStorage.setItem("orderbook_coin_type", '"USDC"');
+          // For mobile orderbook - set to "quote"
+          localStorage.setItem("orderbook_mobile_coin_unit", '"quote"');
+          // localStorage.getItem("orderbook_coin_type")
+          // localStorage.getItem("orderbook_mobile_coin_unit")
+
+        } catch (e) {
+          // ignore
+        }
+      }, 3000);
+    }
+    return () => {
+      if (t) clearTimeout(t);
+    };
+  }, [location.pathname]);
+
 
   useEffect(() => {
     updateSymbol(symbol);
