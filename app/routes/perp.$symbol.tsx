0c6a4573efbfa729587bb5b502a0a95923cb4e8e
Wuzhong <wuzhong@orderly.network>
2025-10-15 19:40:00 +0800

    Remove location.pathname.startsWith("/perp")

--- app/routes/perp.$symbol.tsx
+++ app/routes/perp.$symbol.tsx
@@ -36,21 +36,20 @@ export default function PerpPage() {
   useEffect(() => {
     if (typeof window === "undefined") return;
     let t: number | undefined;
-    if (location.pathname.startsWith("/perp")) {
-      t = window.setTimeout(() => {
-        try {
-          // For desktop orderbook - set to quote (USDC)
-          localStorage.setItem("orderbook_coin_type", '"USDC"');
-          // For mobile orderbook - set to "quote"
-          localStorage.setItem("orderbook_mobile_coin_unit", '"quote"');
-          // localStorage.getItem("orderbook_coin_type")
-          // localStorage.getItem("orderbook_mobile_coin_unit")
 
-        } catch (e) {
-          // ignore
-        }
-      }, 3000);
-    }
+    t = window.setTimeout(() => {
+      try {
+        // For desktop orderbook - set to quote (USDC)
+        localStorage.setItem("orderbook_coin_type", '"USDC"');
+        // For mobile orderbook - set to "quote"
+        localStorage.setItem("orderbook_mobile_coin_unit", '"quote"');
+
+        // localStorage.getItem("orderbook_coin_type")
+        // localStorage.getItem("orderbook_mobile_coin_unit")
+      } catch (e) {
+      }
+    }, 1000);
+
     return () => {
       if (t) clearTimeout(t);
     };
